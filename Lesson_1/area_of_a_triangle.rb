fbce65650b3ecc05282465155bcc25e70e0c69b5
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-01-23 15:56:18 +0200

    area of a triangle

---
+++ Lesson_1/area_of_a_triangle.rb
@@ -0,0 +1,6 @@
+print "Length of triangle base: "
+triangle_base = gets.chomp
+print "Length of triangle height: "
+triangle_height = gets.chomp
+
+puts "Area of a triangle is equal to #{0.5 * triangle_base.to_i * triangle_height.to_i}."
