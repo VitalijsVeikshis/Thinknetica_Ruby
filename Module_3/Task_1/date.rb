03843ec55bf94c324c9b70ea2b6656b1eb6d1dbb
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-01-25 18:58:45 +0200

    date

---
+++ Module_3/Task_1/date.rb
@@ -0,0 +1,13 @@
+print 'Enter please date (dd/mm/yyyy):'
+date = gets.chomp.split('/').map { |value| value.to_i }
+day = date[0]
+month = date[1]
+year = date[2]
+
+months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+
+if ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)
+  months[2] = 29
+end
+
+puts months[0..(month - 1)].reduce(:+) + day
