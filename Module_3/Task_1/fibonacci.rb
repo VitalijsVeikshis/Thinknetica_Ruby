aa130d052f1ae7fb9f9438a4c5a1a34045545968
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-01-26 13:42:27 +0200

    Module 3, attempt 3

--- Module_3/Task_1/fibonacci.rb
+++ Module_3/Task_1/fibonacci.rb
@@ -1,12 +1,7 @@
 fibonacci = [0, 1]
 
-loop do
-  f = fibonacci[-1] + fibonacci[-2]
-  if f < 100
-    fibonacci.push(f)
-  else
-    break
-  end
+while fibonacci[-1] + fibonacci[-2] < 100 do
+  fibonacci.push(fibonacci[-1] + fibonacci[-2])
 end
 
 fibonacci.each_with_index { |number, index| puts "#{index}: #{number}" }
