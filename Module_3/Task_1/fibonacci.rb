2bdb9f5ecb0206bb98ddb186f419e6a5a7156a62
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-01-26 14:02:18 +0200

    Fix fibonacci

--- Module_3/Task_1/fibonacci.rb
+++ Module_3/Task_1/fibonacci.rb
@@ -1,7 +1,7 @@
 fibonacci = [0, 1]
 
-while fibonacci[-1] + fibonacci[-2] < 100 do
-  fibonacci.push(fibonacci[-1] + fibonacci[-2])
+while (f = fibonacci[-1] + fibonacci[-2]) < 100 do
+  fibonacci.push(f)
 end
 
 fibonacci.each_with_index { |number, index| puts "#{index}: #{number}" }
