07f9460e3792b85408f62767109de8246f5607f3
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-01-26 13:55:50 +0200

    Fix vowels

--- Module_3/Task_1/vowels.rb
+++ Module_3/Task_1/vowels.rb
@@ -1,12 +1,8 @@
 vowels_arr = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
-vowels_hash = {
-  'ё': 7
-}
+vowels_hash = {}
 
-('а'..'я').each_with_index do |letter, index|
+('а'..'я').to_a.insert(6, 'ё').each_with_index do |letter, index|
   vowels_hash[letter] = index + 1 if vowels_arr.include?(letter)
 end
-
-vowels_hash = vowels_hash.sort_by{ |letter, index| index }.to_h
 
 vowels_hash.each { |letter, index| puts "#{letter}: #{index}" }
